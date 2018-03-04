4099a539e3724f30860c4eeb77ca08b34374962d
nickscript0 <nickscript0@users.noreply.github.com>
2018-03-04 08:07:33 -0700

    working proxy with validHost check

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,36 @@
 import * as Koa from 'koa';
+import * as request from 'request-promise-native';
+
+import * as tld_test from './regexp-top-level-domain';
 
 const app = new Koa();
 
 
 app.use(async (ctx, next) => {
-  //console.log(ctx.request.url);
-  //ctx.body = `RESPONSE`;
-
-  await next();
+    await handleRequest(ctx);
+    await next();
 });
 
 app.listen(8080);
+
+async function handleRequest(ctx: Koa.Context) {
+    const requestUrl = ctx.request.url.slice(1);
+
+    if (!isValidHost(requestUrl)) {
+        ctx.response.status = 404;
+        ctx.body = `Invalid hostname!! - ${requestUrl}`;
+        return;
+    }
+
+    try {
+        const responseBody = await request.get(requestUrl);
+        ctx.body = responseBody;
+    } catch (e) {
+        ctx.body = e;
+    }
+}
+
+function isValidHost(url: string) {
+    const hasHttp = url.startsWith('http://') || url.startsWith('https://');
+    return hasHttp && tld_test.regexp.test(url);
+}
