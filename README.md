4ab0903f15ad1a3d2b9284cdc0990a640e63a811
nickscript0 <nickscript0@users.noreply.github.com>
2018-03-04 09:02:56 -0700

    add better url validation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Node CORS Proxy
 A nodejs reverse proxy that adds a CORS header to the proxied request.
+
+> Only run this in trusted non-public environments. As there is no restrictions on where requests are proxied from, or when the CORS header is enabled, it is open to XSS attacks.
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import {URL} from 'url';
 import * as Koa from 'koa';
 import * as request from 'request-promise-native';
 
@@ -16,28 +17,41 @@ app.listen(8080);
 async function handleRequest(ctx: Koa.Context) {
     const requestUrl = ctx.request.url.slice(1); // Remove the preceeding /
 
-    const isValidCheck = isValidHost(requestUrl);
+    const isValidCheck = isValidRequest(requestUrl);
     if (!isValidCheck.valid) {
         ctx.response.status = 404;
-        ctx.body = `Invalid hostname!! - ${isValidCheck.reason}`;
+        ctx.body = `Invalid url: ${isValidCheck.reason}`;
         return;
     }
 
     try {
         const responseBody = await request.get(requestUrl);
         ctx.body = responseBody;
+        ctx.response.set('Access-Control-Allow-Origin', '*');
     } catch (e) {
         ctx.body = e;
     }
 }
 
 
-function isValidHost(url: string): { valid: boolean, reason: string } {
-    const hasHttp = url.startsWith('http://') || url.startsWith('https://');
+function isValidRequest(urlString: string): { valid: boolean, reason: string } {
+    let url;
+
+    // Validate url parses
+    try {
+        url = new URL(urlString);
+    } catch (err) {
+        return {valid: false, reason: "Unable to parse url"};
+    }
+
+    // Validate protocol
+    const hasHttp = url.href.startsWith('http://') || url.href.startsWith('https://');
     if (!hasHttp) {
         return { valid: false, reason: 'Does not start with http[s]://' };
     }
-    if (tld_test.regexp.test(url)) {
+
+    // Validate TLD
+    if (!tld_test.regexp.test(url.hostname)) {
         return { valid: false, reason: `Does not have a valid top level domain: ${url}` };
     }
     return { valid: true, reason: "" };
